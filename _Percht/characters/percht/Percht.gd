a19a101aa4a666461e78a1464450485719018fa8
Magneter102 <59408749+Magneter102@users.noreply.github.com>
2024-10-23 23:50:06 +0200

    Fix for hurt?
    Now manually handling the anim for Hurt states because they didn't work otherwise :P

--- _Percht/characters/percht/Percht.gd
+++ _Percht/characters/percht/Percht.gd
@@ -6,8 +6,12 @@ func change_kind(new_kind):
 	kind = new_kind
 
 func _draw():
-	._draw()
 	var curr_state = current_state()
 	
 	if not (curr_state.anim_name.ends_with("Pretty") or curr_state.anim_name.ends_with("Ugly")):
 		curr_state.anim_name += kind
+	
+	if not is_ghost:
+		print(curr_state, curr_state.anim_name)
+	
+	._draw()

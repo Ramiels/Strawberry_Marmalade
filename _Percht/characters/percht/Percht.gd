1a951b0d388a5c0cb73160b90659cefa16b26527
Magneter102 <59408749+Magneter102@users.noreply.github.com>
2024-10-24 00:26:03 +0200

    Change form demo + fix (hack) for sprite updating

--- _Percht/characters/percht/Percht.gd
+++ _Percht/characters/percht/Percht.gd
@@ -7,11 +7,22 @@ func change_kind(new_kind):
 
 func _draw():
 	var curr_state = current_state()
+	var anim_name = curr_state.anim_name
+
 	
-	if not (curr_state.anim_name.ends_with("Pretty") or curr_state.anim_name.ends_with("Ugly")):
-		curr_state.anim_name += kind
+	# This is ATROCIOUS.
+	if not anim_name.ends_with("BOTH"):
+		if anim_name.ends_with("Pretty"):
+			anim_name.erase(anim_name.length() - 6, 6)
+		if anim_name.ends_with("Ugly"):
+			anim_name.erase(anim_name.length() - 4, 4)
+		
+		anim_name += kind
 	
-	if not is_ghost:
-		print(curr_state, curr_state.anim_name)
+	curr_state.anim_name = anim_name
+	
+	#if not is_ghost:
+	#	print("_draw() ", kind)
+	#	print(curr_state, curr_state.anim_name)
 	
 	._draw()

0bd6e2882daf63eb5b1495a3482cfd1c806925f9
Magneter102 <59408749+Magneter102@users.noreply.github.com>
2024-11-06 14:30:53 +0100

    Pointy Shoe and Goodie Bag changes

--- _Percht/characters/percht/projectiles/GoodieBag.gd
+++ _Percht/characters/percht/projectiles/GoodieBag.gd
@@ -2,7 +2,7 @@ extends BaseProjectile
 
 var MANDARIN_PROJ = preload("res://_Percht/characters/percht/projectiles/Mandarin.tscn")
 var PEANUTS = preload("res://_Percht/characters/percht/projectiles/PeanutsParticles.tscn")
-var dirs = [["2.0", "-4.0"], ["2.0", "-2.0"], ["5.0", "-1.0"]]
+var dirs = [["2.0", "-4.0", "1.2"], ["2.0", "-2.0", "0.9"], ["5.0", "-1.0", "0.7"]]
 
 func disable():
 	creator.goodie_bag = null
@@ -11,8 +11,11 @@ func disable():
 func explode():
 	for dir in dirs:
 		var obj = spawn_object(MANDARIN_PROJ, 0, 0)
-		obj.set_vel(dir[0], dir[1])
+		obj.set_vel(fixed.mul(dir[0], str(get_facing_int())), dir[1])
+		
+		var extra_force = fixed.mul(str(get_vel().y), dir[2])
+		obj.apply_force("0.0", extra_force)
+		
 		spawn_particle_effect_relative(PEANUTS, Vector2(0, 0))
-
 		
 		disable()
--- _Percht/characters/percht/states/PointyShoe.gd
+++ _Percht/characters/percht/states/PointyShoe.gd
@@ -6,19 +6,19 @@ onready var hitbox = $Hitbox
 
 
 
-func _frame_3():
-	host.start_projectile_invulnerability()
-
-func _frame_5():
-	if not got_hit:
-		host.has_hyper_armor = true
-
-func _frame_12():
-	host.has_hyper_armor = false
-	host.end_projectile_invulnerability()
-
-func on_got_hit():
-	hitbox.hitstun_ticks = 20
-	got_hit = true
-	feinting = false
-	host.feinting = false
+#func _frame_4():
+#	host.start_projectile_invulnerability()
+#
+#func _frame_8():
+#	if not got_hit:
+#		host.has_hyper_armor = true
+#
+#func _frame_12():
+#	host.has_hyper_armor = false
+#	host.end_projectile_invulnerability()
+#
+#func on_got_hit():
+#	hitbox.hitstun_ticks = 20
+#	got_hit = true
+#	feinting = false
+#	host.feinting = false

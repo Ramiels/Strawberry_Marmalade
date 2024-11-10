60a709304d048e10f67152070254cc1cb75757cc
heeruv <y.lakchili@gmail.com>
2024-11-10 14:07:22 +0100

    A lot of base sprites, pretty aerial,pretty hustle
    it was fun having a job

--- _Percht/characters/percht/states/ChangeForm.gd
+++ _Percht/characters/percht/states/ChangeForm.gd
@@ -1,6 +1,6 @@
 extends "res://_Percht/PerchtMove.gd"
 
-func _frame_3():
+func _frame_5():
 	print(host.kind)
 	if host.kind == "Pretty":
 		host.change_kind("Ugly")
---
+++ _Percht/characters/percht/states/HustlePretty.gd
@@ -0,0 +1,11 @@
+extends "res://_Percht/PerchtMove.gd"
+
+var can_apply_sadness = false
+var is_whip_move = true
+
+func _enter():
+	can_apply_sadness = host.combo_count <= 0
+
+func _frame_44():
+	host.gain_super_meter_raw(host.MAX_SUPER_METER)
+	host.unlock_achievement("ACH_HUSTLE", true)

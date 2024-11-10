bf1624ecf995aaa5867306bb9dc65377f9b9248d
heeruv <y.lakchili@gmail.com>
2024-11-10 16:27:46 +0100

    rolls and ugly grab, half of ugly hustle

--- _Percht/characters/percht/states/HustleUgly.gd
+++ _Percht/characters/percht/states/HustleUgly.gd
@@ -2,12 +2,13 @@ extends "res://_Percht/PerchtMove.gd"
 
 
 var can_apply_sadness = false
-var is_whip_move = true
 
 func _enter():
 	can_apply_sadness = host.combo_count <= 0
 
+func _frame_29():
+	host.get_opponent().gain_super_meter_raw(host.get_opponent().MAX_SUPER_METER)
+
 func _frame_44():
 	host.gain_super_meter_raw(host.MAX_SUPER_METER)
 	host.unlock_achievement("ACH_HUSTLE", true)
-	

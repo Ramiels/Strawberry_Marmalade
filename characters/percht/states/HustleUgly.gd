d4a3918a1922b1bd7ae6622ffc723a16e5029ad0
Maximilian Kriegner <kriegner.maximilian@gmail.com>
2025-03-10 20:20:29 +0100

    ugly hustle script unfinished, sfx not working yet
    swapped out mash claw sprites and changed mash claw whiff sounds, also adjusted ugly hustle frames

--- characters/percht/states/HustleUgly.gd
+++ characters/percht/states/HustleUgly.gd
@@ -6,6 +6,24 @@ var can_apply_sadness = false
 func _enter():
 	can_apply_sadness = host.combo_count <= 0
 
+func _frame_4():
+	host.play_sound("Block")
+	pass
+
+#swish sfx not playing, needs fixing
+func _frame_16():
+	host.play_sound("Swish")
+	pass
+
+func _frame_24():
+	host.play_sound("Swish")
+	pass
+	
+func _frame_32():
+	host.play_sound("Swish")
+	pass
+
+
 func _frame_44():
 	host.gain_super_meter_raw(host.MAX_SUPER_METER)
 	host.unlock_achievement("ACH_HUSTLE", true)

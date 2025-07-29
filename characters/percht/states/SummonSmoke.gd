570d1ec0b951bb419a94b8fd6ff1dbf6dc809c18
Magneter102 <59408749+Magneter102@users.noreply.github.com>
2025-07-29 20:19:18 +0200

    Smokescreen rework

--- characters/percht/states/SummonSmoke.gd
+++ characters/percht/states/SummonSmoke.gd
@@ -1,8 +1,9 @@
 extends "res://_Percht/PerchtMove.gd"
 
 var PROJ_SCENE = preload("res://_Percht/characters/percht/projectiles/Smoke.tscn")
-var number = 6 # must be even
+var number = 4 # must be even
 var distance = 55
+var force_mult = "5"
 
 #func _frame_1():
 	#print(host.smoke_projectiles)
@@ -10,19 +11,14 @@ var distance = 55
 	#	host.objs_map[smoke].smokescreen_disable()
 
 func _frame_6():
-	var vec = xy_to_dir(data.x, data.y)
-	var dir = fixed.normalized_vec(str(data.x), str(data.y))
-	var start_x = fixed.mul(vec.x, str(distance * number/2))
-	var start_y = fixed.mul(vec.y, str(distance * number/2))
-	start_x = fixed.sub(start_x, fixed.mul(str(distance * number/2), dir.x))
-	start_y = fixed.sub(start_y, fixed.mul(str(distance * number/2), dir.y))
-	start_y = fixed.sub(start_y, "20")
+	var vec_start = xy_to_dir(data["Start"].x, data["Start"].y)
+	var vec_end = xy_to_dir(data["End"].x, data["End"].y)
+	var delta = fixed.vec_sub(vec_end.x, vec_end.y, vec_start.x, vec_start.y)
 	
 	for i in range(number):
-		var move_by = fixed.vec_mul(dir.x, dir.y, str(distance * i))
-		var pos = fixed.vec_add(start_x, start_y, move_by.x, move_by.y)
-		if int(floor(float(pos.y))) > -20:
-			pos.y = -20
-		var smoke = host.spawn_object(PROJ_SCENE, int(floor(float(pos.x))), int(floor(float(pos.y))), false)
-		host.smoke_projectiles.append(smoke.obj_name)
-		smoke.smokescreen = true
+		var mult = fixed.div(str(i), str(number - 1))
+		var new_target = fixed.vec_add(vec_start.x, vec_start.y, fixed.mul(delta.x, mult), fixed.mul(delta.y, mult))
+		var force = fixed.vec_mul(new_target.x, new_target.y, force_mult)
+		
+		var smoke = host.spawn_object(PROJ_SCENE, 20, -20, true)
+		smoke.apply_force_relative(force.x, force.y)
